4539d2d4394e5025d97635ff9f11293a62eb3380
Megha Sarkar <meghasarkar0893@gmail.com>
2022-01-17 12:21:02 +0530

    CSV to Parquet

--- Important_Python_Snippets/general/csvtoparq.py
+++ Important_Python_Snippets/general/csvtoparq.py
@@ -1,22 +1,21 @@
 import os
 import pandas as pd
 
+# Provide paths for base path for CSV file and CSV filename
+BASE_DIR = r'C:\Users\megha\Documents\Python Programming\Input Testdata'
+FILE_CSV = '\CSV\data-1.csv'
 
-#Provide paths for base path for CSV file and CSV filename
-BASE_DIR=r'C:\Users\megha\Documents\Python Programming\Input Testdata'
-FILE_CSV='\CSV\data-1.csv'
+# User should create a Parquet folder from base path
+# A parameter is set to Parquet path
+FILE_PATH_PARQUET = BASE_DIR + r'\PARQUET'
 
-#User should create a Parquet folder from base path
-#A parameter is set to Parquet path
-FILE_PATH_PARQUET=BASE_DIR+r'\PARQUET'
-
-#Extract the CSV file and split to the exact filename without extension so that
+# Extract the CSV file and split to the exact filename without extension so that
 # the parquet file name is similar to the exact csv
-CSV_FILE=BASE_DIR+FILE_CSV
+CSV_FILE = BASE_DIR + FILE_CSV
 filename = os.path.basename(CSV_FILE)
 
 split_tup = os.path.splitext(filename)
-PARQUET_FILE=FILE_PATH_PARQUET+'\\'+split_tup[0]+'.parquet'
+PARQUET_FILE = FILE_PATH_PARQUET + '\\' + split_tup[0] + '.parquet'
 
 
 def write_parquet_file():
@@ -28,4 +27,3 @@ write_parquet_file()
 
 df = pd.read_parquet(PARQUET_FILE)
 print(df)
-
